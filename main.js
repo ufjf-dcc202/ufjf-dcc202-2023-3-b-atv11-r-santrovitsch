f8c019efd715da922a98440c4a69e72039a59e5d
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-11-21 22:51:08 -0300

    Adiciona atualizar lista

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+import {getlista} from "../src/lista.js";
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-adicionaElementoNaLista("Primeiro");
-adicionaElementoNaLista("Segundo");
-adicionaElementoNaLista("Terceiro");
+atualizarListaOrdenada()
 
-function adicionaElementoNaLista(texto){
+function atualizarListaOrdenada(){
+    const lista = getLista();
+    for(let i=0; i < lista.lenght;i++){
+        adicionaElementoNaListaOrdenada(lista[i]);
+    }
+
+}
+
+
+function adicionaElementoNaListaOrdenada(texto){
     const li = document.createElement("li");
     li.textContent = texto;
     olItens.appendChild(li);
