023a8ef9425979e5df71c16289947a94faa06bf3
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-11-21 23:12:06 -0300

    MODIFICACAO

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getLista, adicionaNaLista } from "./src/lista.js";
+import { getLista, adicionaNaLista, limpaLista } from "./src/lista.js";
 
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
