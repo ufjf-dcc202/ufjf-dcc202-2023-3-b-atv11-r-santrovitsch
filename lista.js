a634f1b03c8b365f9ad828ed9ab102b6c7298899
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-11-21 22:30:22 -0300

    Adicionei funções na lista

---
+++ lista.js
@@ -0,0 +1,23 @@
+const lista = {
+    "Um", 
+    "Dois", 
+    "Três"
+};
+
+function getLista(){
+    return lista;
+}
+
+function adicionaNaLista(){
+
+}
+
+function removeDaLista(){
+
+}
+
+function limpaLista(){
+
+}
+
+export {getLista, adicionaNaLista, removeDaLista, limpaLista};
